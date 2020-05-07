adaae4c5348f86faf1f175970c7394b54fb50ec9
Dusan <xelcina@gmail.com>
2020-05-07 22:25:00 +0200

    [1.0.0] Initial commit

---
+++ src/main/java/com/tusofia/internshipprogram/InternshipProgramApplication.java
@@ -0,0 +1,13 @@
+package com.tusofia.internshipprogram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternshipProgramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternshipProgramApplication.class, args);
+	}
+
+}
